8500af4299c573ab0aeea871d61a431381aadbac
Greg Dunne <gregdunne89@hotmail.com>
2017-03-11 11:45:01 +0000

    First upload
    Test to ensure it's done correctly.

---
+++ _scripts/moveBehaviourGregTarget.cs
@@ -0,0 +1,74 @@
+﻿//moveBehaviourGregTarget.cs
+//@author: Greg Dunne
+//Date 06.03.2017
+
+using UnityEngine;
+using System.Collections;
+
+public class moveBehaviourGregTarget : MonoBehaviour {
+	public int targetHealth;
+	private GameObject arena;
+	Arena arenaScript;
+	public GameObject explosion;
+	public Vector3 position;
+	public Vector3 localScale;
+	public Vector3 rotate;
+
+
+
+	// Use this for initialization of all variables which need to have values at game start
+	void Start () {
+		targetHealth = 4;
+
+		//fetching the arena GameObject and arena class
+		arena = GameObject.FindGameObjectWithTag("arena");
+		arenaScript = arena.GetComponent<Arena> ();
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+		transform.Rotate(0,0,0.5f);// rotates slowly
+		transform.localScale = new Vector3(1f, 1f, 0);//decreases size so it's not rotating outside position
+
+	
+	}
+
+	//on collision with the ball:
+	void OnTriggerEnter(Collider other) {
+		//transform.position = new Vector3 (0, 1.5f,0.1f);
+		//transform.localScale += new Vector3(0.2f, 0.2f, 0);
+
+		if (other.CompareTag ("ball")) {
+			//subtract target health
+			targetHealth--;
+
+			//if target health is 0:
+			if (targetHealth == 4) {
+				transform.position = new Vector3 (0, 0.7f, 11.9f);
+			}
+			if (targetHealth ==3){
+				transform.position = new Vector3 (2.15f, 0.7f, 11.9f);
+			}
+			if (targetHealth ==2){
+				transform.position = new Vector3 (-2.15f, 2.3f, 11.9f);
+			}
+			if (targetHealth ==1){
+				transform.position = new Vector3 (2.15f, 2.3f, 11.9f);
+			}
+			else if(targetHealth < 1){
+				//subtract 1 target from target counter  
+				arenaScript.removeTarget ();
+				//finally destroy this target object to free up memory 
+				Destroy (gameObject);
+
+
+			}
+		}
+	}
+
+
+
+}
+
---
+++ _scripts/perkSettings.cs
@@ -0,0 +1,67 @@
+﻿//perkSettings.cs
+//@author: Greg Dunne
+//Date 06.03.2017
+
+using UnityEngine;
+using System.Collections;
+
+public class perkSettings : MonoBehaviour {
+	public int perkHealth;
+	public GameObject arena;
+	public GameObject[] backTargets;
+	Arena arenaScript;
+	GameObject[] gameObjects;
+	public int destroyTime = 3;
+	public int startTime=3;
+
+	// Use this for initialization of all variables which need to have values at game start
+	void Start () {
+		//targetHealth = 3;
+
+		//fetching the arena class
+		arena = GameObject.FindGameObjectWithTag("arena");
+		arenaScript = arena.GetComponent<Arena> ();
+		Invoke("perk", startTime);
+
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+		Destroy (gameObject, destroyTime);
+	}
+
+
+		
+
+	void OnTriggerEnter(Collider other) {
+		if (other.CompareTag ("ball")) {
+			perkHealth--;
+			if (perkHealth < 1) {
+				Destroy (gameObject);
+
+				/*
+				//subtract 1 target from target counter 
+				arenaScript.removeTarget ();
+				Destroy(GameObject.Find("back_target_simple(Clone)"));
+				//subtract 1 target from target counter 
+				arenaScript.removeTarget ();
+				*/
+
+				// Destroys all the targetrs which I've given a tag of 'target'
+				gameObjects = GameObject.FindGameObjectsWithTag ("target");
+
+				for(var i = 0 ; i < gameObjects.Length ; i ++)
+				{
+					Destroy(gameObjects[i]);
+					arenaScript.removeTarget ();
+				}
+
+				//This causes the exit to appear when hitting the perk.
+			}
+
+
+		}
+	}
+}
