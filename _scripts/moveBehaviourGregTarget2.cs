95a6cc91df68eb1f4de786229fc2b057d0262d60
Greg Dunne <gregdunne89@hotmail.com>
2017-03-16 19:24:09 +0000

    Moving Target
    Moving target for scene 2

--- _scripts/moveBehaviourGregTarget2.cs
+++ _scripts/moveBehaviourGregTarget2.cs
@@ -1,11 +1,14 @@
 ﻿//moveBehaviourGregTarget.cs
 //@author: Greg Dunne
-//Date 06.03.2017
+//Date 16.03.2017
 
 using UnityEngine;
 using System.Collections;
 
 public class moveBehaviourGregTarget2 : MonoBehaviour {
+
+
+
 	public int targetHealth;
 	private GameObject arena;
 	Arena arenaScript;
@@ -14,31 +17,53 @@ public class moveBehaviourGregTarget2 : MonoBehaviour {
 	public Vector3 localScale;
 	public Vector3 rotate;
 
-	public Transform center;
-	public float degreesPerSecond = -65.0f;
+	public float speed;
+	public GameObject rightWall;
+	public GameObject leftWall;
+	public bool reverseMove = false;
 
-	private Vector3 v;
+	private float startTime;
+	private float journeyLength;
+
 
 	// Use this for initialization of all variables which need to have values at game start
 	void Start () {
-		targetHealth = 4;
+		targetHealth = 1;
 
 		//fetching the arena GameObject and arena class
 		arena = GameObject.FindGameObjectWithTag("arena");
 		arenaScript = arena.GetComponent<Arena> ();
 
-
-
+		//Moving the target
+		startTime = Time.time;
+		journeyLength = Vector3.Distance (leftWall.transform.position, rightWall.transform.position);
 	}
 
 	// Update is called once per frame
 	void Update () {
 
+		float distCovered = (Time.time - startTime) * speed;
+		float fracJourney = distCovered / journeyLength;
 
-
-	
+		//go from right to left
+		if (reverseMove) {
+			transform.position = Vector3.Lerp (rightWall.transform.position, leftWall.transform.position, fracJourney);
+		}else{
+			//go from left to right
+			transform.position = Vector3.Lerp (leftWall.transform.position, rightWall.transform.position, fracJourney);
 	}
+		//bounce
+		if ((Vector3.Distance(transform.position, rightWall.transform.position)==0.0f || Vector3.Distance(transform.position, leftWall.transform.position)==0.0f)){
+			if (reverseMove){
+				reverseMove=false;
+			}
+			else{
+				reverseMove = true;
+			}
+			startTime = Time.time;
+		}
 
+	}
 
 
 	//on collision with the ball:
@@ -51,20 +76,7 @@ public class moveBehaviourGregTarget2 : MonoBehaviour {
 			targetHealth--;
 
 			//if target health is 0:
-			if (targetHealth == 4) {
-				transform.position = new Vector3 (0, 0.7f, 11.9f);
-				transform.Rotate (0, 0, 0.5f);
-			}
-			if (targetHealth ==3){
-				transform.position = new Vector3 (2.15f, 0.7f, 11.9f);
-			}
-			if (targetHealth ==2){
-				transform.position = new Vector3 (-2.15f, 2.3f, 11.9f);
-			}
-			if (targetHealth ==1){
-				transform.position = new Vector3 (2.15f, 2.3f, 11.9f);
-			}
-			else if(targetHealth < 1){
+				if(targetHealth < 1){
 				//subtract 1 target from target counter  
 				arenaScript.removeTarget ();
 				//finally destroy this target object to free up memory 
